9374d45f2e959740ab4c4525a2f585870c67173b
diorblack <45602451+claudy1337@users.noreply.github.com>
2022-10-29 18:28:12 +0300

    Добавьте файлы проекта.

---
+++ AdditionalEducation/Data/Classes/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdditionalEducation.Data.Model;
+using System.Threading.Tasks;
+
+namespace AdditionalEducation.Data.Classes
+{
+    internal class DBConnection
+    {
+        public static schoolEntities connect = new schoolEntities();
+    }
+}
---
+++ AdditionalEducation/Data/Classes/DBMethodsFromUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdditionalEducation.Data.Model;
+
+namespace AdditionalEducation.Data.Classes
+{
+    internal class DBMethodsFromUser
+    {
+        public static ObservableCollection<User> GetUsers()
+        {
+            return new ObservableCollection<User>(DBConnection.connect.User);
+        }
+        public static IEnumerable<User> GetUser()
+        {
+            return GetUsers().ToList();
+        }
+        public static bool IsCorrectUser(string login, string password)
+        {
+            ObservableCollection<User> users = new ObservableCollection<User>(DBConnection.connect.User);
+            var currentUser = users.Where(u => u.Authorization.Password == password && u.Authorization.Login == login).FirstOrDefault();
+            return currentUser != null;
+        }
+        public static bool isRole(string login)
+        {
+            ObservableCollection<User> users = new ObservableCollection<User>(DBConnection.connect.User);
+            var isAdmin = users.Where(u => u.Authorization.Login == login && u.RoleID == 1);
+            return isAdmin != null;
+        }
+    }
+}
